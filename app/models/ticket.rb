6fffdddbce6563fba91ad0097df9312d70889a3f
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-01-03 16:51:20 -0500

    added serializer, seeded db with sample data, and added uuid method to tickets to generate on creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,2 +1,10 @@
 class Ticket < ApplicationRecord
+    belongs_to :game
+    belongs_to :user
+
+    before_create :unique_code_maker
+
+    def unique_code_maker
+        self.ticket_code = SecureRandom.uuid
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,63 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+games = Game.create([
+    {
+        sport: "Football",
+        gender: "boys",
+        level: "Varsity",
+        home_team: "Paint Valley",
+        visiting_team: "Adena",
+        max_capacity: "1000",
+        event_date: DateTime.new(2020, 9, 1),
+        price: 500,
+    },
+
+    {
+        sport: "Football",
+        gender: "boys",
+        level: "Varsity",
+        home_team: "Paint Valley",
+        visiting_team: "Unioto",
+        max_capacity: "1000",
+        event_date: DateTime.new(2020, 9, 8),
+        price: 500,
+    },
+    {
+        sport: "Volleyball",
+        gender: "girls",
+        level: "JV",
+        home_team: "Paint Valley",
+        visiting_team: "Piketon",
+        max_capacity: "270",
+        event_date: DateTime.new(2020, 9, 5),
+        price: 700,
+    }
+])
+
+users = User.create([
+    {
+        first_name: "Greatest",
+        last_name: "Ever",
+        email: "my@email.com",
+        cellphone: "1234567890",
+        password: "12345",
+    },
+    {
+        first_name: "Jeorge",
+        last_name: "Simpilton",
+        email: "jeo@email.com",
+        cellphone: "2345678901",
+        password: "pas3wrid",
+    }
+])
+
+tickets = Ticket.create([
+    {
+        game: games.first,
+        user: users.first,
+        used: false
+    },
+    {
+        game: games.first,
+        user: users.second,
+        used: false
+    }
+])
