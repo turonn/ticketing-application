af91f8dea335cdf729fe0ce85b9deeb8200195c9
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-01-05 21:44:13 -0500

    db:migrated the devise migration. removed password from user and added validation to email column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-    has_many :tickets
+  # Include default devise modules. Others available are:
+  # :lockable, :timeoutable, :trackable and :omniauthable
+  before_action :authenticate_user!
+  
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+  
+  has_many :tickets
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'hello_world', to: 'r#index'
   root 'welcome#index'
 
