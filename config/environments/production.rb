cbc10006f57158c5e9c424cf11a1449424d6881b
Nicholas Turon <nicholasturon@Nicholass-MacBook-Pro.local>
2021-01-05 20:53:11 -0500

    installed Devise and added default_url for action mailers in both config/enviornments  development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ticketing_application_production"
 
   config.action_mailer.perform_caching = false
+  #Devise default mailer host 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
